2bd941eb2df4edf2004276e114c71ca8f8777698
Kevin Hall <khall@liveops.com>
2013-02-15 18:28:45 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fire::Application.config.secret_token = '57d1eb708eb11c7605f85ba53ec50989ba3df800c2dc3643521938d3c86b2e894c637e7e0b0b1e69434f791e0c47e9e72c06928107081ca23f4112431f3ed7d0'
---
+++ fire_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FireTest::Application.config.secret_token = 'c308b158738959c13359538efdeebc882ee71f399f06d06712616f61b529c3beb6e7796b8bf98be12f795f52cf9ed2eafa47900cc278b12bc42d935bfde2699e'
