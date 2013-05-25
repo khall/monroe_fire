533119a693a64e62bc7643cf88896e5ffbb0faaa
Kevin Hall <khall@liveops.com>
2013-05-24 17:24:37 -0700

    Fixing view spec

--- spec/views/compartments/edit_spec.rb
+++ spec/views/compartments/edit_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "compartments/edit" do
   describe "edit a compartment" do
     it "renders edit for a compartment" do
-      compartment = Fabricate(:compartment, description: "Driver's side rear")
+      compartment = Fabricate(:compartment, description: "Drivers side rear")
       assign(:compartment, compartment)
       render
       rendered.should =~ /#{compartment.description}/
