bc5e0edd8b64200b74de99ad7c87d8ec4a5784ae
Kevin Hall <khall@liveops.com>
2014-03-02 12:17:03 -0800

    Make complete be the first setting, in-progress the second for certifications

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -8,22 +8,22 @@ class CertificationsController < ApplicationController
 
   def update_progress
     @certification = Certification.find(params[:id])
-    if @certification.progress == "in-progress"
+    if @certification.progress == "incomplete"
       @certification.update_attribute(:progress, "complete")
-      render :js => "$('#td_#{@certification.id}').removeClass('in-progress').addClass('complete')"
-    elsif @certification.progress == "complete"
+      render :js => "$('#td_#{@certification.id}').removeClass('incomplete').addClass('complete')"
+    elsif @certification.progress == "in-progress"
       @certification.update_attribute(:progress, "incomplete")
-      render :js => "$('#td_#{@certification.id}').removeClass('complete').addClass('incomplete')"
+      render :js => "$('#td_#{@certification.id}').removeClass('in-progress').addClass('incomplete')"
     else
       @certification.update_attribute(:progress, "in-progress")
-      render :js => "$('#td_#{@certification.id}').removeClass('incomplete').addClass('in-progress')"
+      render :js => "$('#td_#{@certification.id}').removeClass('complete').addClass('in-progress')"
     end
   end
 
   def gen
-    c = Certification.create(user_id: params[:user_id], course_id: params[:course_id], progress: "in-progress")
+    c = Certification.create(user_id: params[:user_id], course_id: params[:course_id], progress: "complete")
     dom_id = "#td_#{params[:user_id]}_#{params[:course_id]}"
-    render :js => "$('#{dom_id}').removeClass('incomplete').addClass('in-progress');
+    render :js => "$('#{dom_id}').removeClass('incomplete').addClass('complete');
                    $('#{dom_id}').children().attr('href', 'certifications/#{c.id}/update_progress');
                    $('#{dom_id}').attr('id', 'td_#{c.id}');"
   end
